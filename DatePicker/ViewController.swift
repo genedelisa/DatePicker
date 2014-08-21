907caeb891df67790d0faecca7723544ddaf145a
Gene De Lisa <gene@rockhoppertech.com>
2014-08-21 13:42:53 -0400

    Initial

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,17 +9,128 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var toolbar: UIToolbar!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    var theDate:NSDate! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    var datePicker = UIDatePicker()
+    
+    var pickerDateToolbar:UIToolbar!
+    
+    var formatter:NSDateFormatter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.FullStyle
+        self.theDate = NSDate.date()
+        
 
+        var chooseDateButton = UIBarButtonItem(title: NSLocalizedString("Choose Date", comment: ""), style: .Plain, target: self, action: "addPicker:")
+        
+        var spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+            target: self, action: nil)
+        self.toolbar.setItems( [chooseDateButton], animated: true)
+        
+        configDatePicker()
+    }
+    
+    func updateUI() {
+        dateLabel.text = formatter.stringFromDate(theDate)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var blurView:UIVisualEffectView!
+    func blur() {
+        // ios 8 stuff
+        blurView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+        blurView.userInteractionEnabled = false
+        self.view.addSubview(blurView)
+        blurView.frame = self.view.bounds
+    }
+    
+    func unblur() {
+        self.blurView.removeFromSuperview()
+    }
+    
+    func configDatePicker() {
+        datePicker.alpha = 1.0
+        datePicker.backgroundColor = UIColor(red: 0, green: 0.9, blue: 0.9, alpha: 1.0)
+        //        datePicker.addTarget(self,
+        //            action:"datePickerDateChanged:",
+        //            forControlEvents:.ValueChanged)
 
+        datePicker.datePickerMode = .Date
+        
+        pickerDateToolbar = UIToolbar()
+        
+        let dateComponents = NSDateComponents()
+        dateComponents.day = 31
+        dateComponents.month = 12
+        dateComponents.year = 2014
+        datePicker.maximumDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+        dateComponents.day = 1
+        dateComponents.month = 1
+        dateComponents.year = 2014
+        datePicker.minimumDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+    }
+    
+    func datePickerDateChanged(dp:UIDatePicker) {
+        self.theDate = dp.date
+    }
+    
+    /**
+    called from toolbar button.
+    */
+    func addPicker(sender : AnyObject) {
+        
+        self.blur()
+        self.datePicker.date = self.theDate
+        
+        var screenRect = self.view.frame
+        var pickerSize = self.datePicker.sizeThatFits(CGSizeZero)
+        var x = screenRect.origin.x + (screenRect.size.width / 2) - (pickerSize.width / 2)
+        var pickerRect = CGRectMake(x,
+            screenRect.origin.y + (screenRect.size.height / 2) - (pickerSize.height / 2),
+            pickerSize.width,
+            pickerSize.height)
+        self.datePicker.frame = pickerRect
+        
+        var toolbarSize = self.pickerDateToolbar.sizeThatFits(CGSizeZero)
+        pickerDateToolbar.frame = CGRectMake(x,
+            pickerRect.origin.y + pickerRect.size.height, // right under the picker
+            pickerSize.width, // make them the same width
+            toolbarSize.height)
+        
+        var done = UIBarButtonItem(title:  NSLocalizedString("Done", comment: ""), style: .Plain, target: self, action: "doneWithDatePicker:")
+        var spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace,
+            target: self, action: nil)
+        pickerDateToolbar.items = [spacer,done,spacer]
+        
+        self.view.addSubview(self.datePicker)
+        self.view.addSubview(self.pickerDateToolbar)
+    }
+    
+    func doneWithDatePicker(sender : AnyObject) {
+        self.datePicker.removeFromSuperview()
+        self.pickerDateToolbar.removeFromSuperview()
+        unblur()
+        self.theDate = self.datePicker.date
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,21 @@
+# Date Picker
+
+Showing a UIDatePicker in the middle of the screen with a blurred background
+
+## Blog post for this example.
+
+[Blog post](http://www.rockhoppertech.com/blog/)
+
+
+
+## Bugs
+
+
+
+## Licensing
+
+I'd appreciate an ack somehow.
+
+## Credits
+
+*	[Gene De Lisa](http://rockhoppertech.com/blog/)
